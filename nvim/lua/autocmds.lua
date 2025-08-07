710b027472d3073e826e7c72355593168df70aa8
Dinno <tahadlrb7@gmail.com>
2025-08-08 00:00:31 +0330

    refactor(nvim): move autocmd functions to utils.lua

--- nvim/lua/autocmds.lua
+++ nvim/lua/autocmds.lua
@@ -1,4 +1,5 @@
 local autocmd = vim.api.nvim_create_autocmd
+local utils = require "utils"
 
 -- INFO: Close all folds in entering buffer
 -- autocmd("BufRead", {
@@ -51,74 +52,12 @@ autocmd("TextYankPost", {
   end,
 })
 
--- INFO: Function to convert binary data to hex view
-local function binary_to_hex(binary_string)
-  local hex_view = ""
-  local ascii_view = ""
-  local address = 0
-
-  for i = 1, #binary_string, 16 do
-    -- Address
-    hex_view = hex_view .. string.format("%08x: ", address)
-
-    -- Hex values
-    for j = i, math.min(i + 15, #binary_string) do
-      local byte = string.byte(binary_string:sub(j, j))
-      hex_view = hex_view .. string.format("%02x ", byte)
-
-      -- ASCII representation
-      if byte >= 32 and byte <= 126 then
-        ascii_view = ascii_view .. string.char(byte)
-      else
-        ascii_view = ascii_view .. "."
-      end
-    end
-
-    -- Pad hex view if less than 16 bytes
-    if #binary_string - i + 1 < 16 then
-      hex_view = hex_view .. string.rep("   ", 16 - (#binary_string - i + 1))
-    end
-
-    hex_view = hex_view .. " " .. ascii_view .. "\n"
-    ascii_view = ""
-    address = address + 16
-  end
-
-  return hex_view
-end
-
--- INFO: Function to check if file is binary
-local function is_binary(filename)
-  local file = io.open(filename, "rb")
-  if not file then
-    return false
-  end
-
-  local chunk = file:read(1024)
-  file:close()
-
-  if not chunk then
-    return false
-  end
-
-  -- Check for null bytes or high number of non-printable characters
-  local non_printable = 0
-  for i = 1, #chunk do
-    local byte = string.byte(chunk:sub(i, i))
-    if byte == 0 or (byte < 32 and byte ~= 9 and byte ~= 10 and byte ~= 13) then
-      non_printable = non_printable + 1
-    end
-  end
-
-  return (non_printable / #chunk) > 0.3
-end
-
 -- INFO: Autocmd to open binary files in hex view
 autocmd("BufReadPost", {
   pattern = "*",
   callback = function()
     local filename = vim.api.nvim_buf_get_name(0)
-    if is_binary(filename) then
+    if utils.isBinary(filename) then
       -- Read file contents
       local file = io.open(filename, "rb")
       if not file then
@@ -128,7 +67,7 @@ autocmd("BufReadPost", {
       file:close()
 
       -- Convert to hex view
-      local hex_content = binary_to_hex(content)
+      local hex_content = utils.binaryToHex(content)
 
       -- Replace buffer content with hex view
       vim.api.nvim_buf_set_lines(0, 0, -1, false, vim.split(hex_content, "\n"))
