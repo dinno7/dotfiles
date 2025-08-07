61b004bec3ba88dde6af643ff5a4bd5aff9901a3
Dinno <tahadlrb7@gmail.com>
2025-08-08 03:07:41 +0330

    fix(nvim): remove redundant eslint linting config

--- nvim/lua/configs/conform.lua
+++ nvim/lua/configs/conform.lua
@@ -1,6 +1,5 @@
 local options = {
   formatters_by_ft = {
-    ["_"] = { "trim_whitespace" },
     javascript = { "prettierd" },
     typescript = { "prettierd" },
     javascriptreact = { "prettierd" },
