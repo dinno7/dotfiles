8a5b4848556d84a6405d593dbeaffb4dc253f346
dinno <tahadlrb7@gmail.com>
2025-08-27 10:31:32 +0330

    feat(nvim): tcd to parent dir if provided with nvim command
    nvim /path/to/dir/file.txt -> it is open file and tcd to /path/to/dir

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -32,8 +32,25 @@ require("lazy").setup({
 dofile(vim.g.base46_cache .. "defaults")
 dofile(vim.g.base46_cache .. "statusline")
 
+-- load autocmds
 require "nvchad.autocmds"
 require "autocmds"
+
+-- Change nvim tcd path to first arg path if provided
+local nargs = vim.fn.argc()
+if nargs == 1 then
+  local arg = vim.fn.argv(0)
+  if type(arg) == "string" and arg ~= "" then
+    local stat = vim.uv.fs_stat(arg) -- nil | uv.uv_fs_t
+    if stat then
+      if stat.type == "directory" then
+        vim.cmd.cd(arg)
+      elseif stat.type == "file" then
+        vim.cmd.cd(vim.fn.fnamemodify(arg, ":h"))
+      end
+    end
+  end
+end
 
 vim.schedule(function()
   require "mappings"
