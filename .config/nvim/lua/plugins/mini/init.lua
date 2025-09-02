1a561e772ae28f2eac786dd94ab684a97a8302a6
dinno <tahadlrb7@gmail.com>
2025-09-02 16:12:29 +0330

    refactor(nvim,mini): move all mini plugins to plugins/mini direcotry

---
+++ .config/nvim/lua/plugins/mini/init.lua
@@ -0,0 +1,8 @@
+return {
+  require "plugins.mini.mini-ai",
+  require "plugins.mini.mini-cursorword",
+  require "plugins.mini.mini-jump2d",
+  require "plugins.mini.mini-move",
+  require "plugins.mini.mini-notify",
+  require "plugins.mini.mini-surround",
+}
