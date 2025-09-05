78d8a7431a5d615aa649e59bd4caae7fe03311ae
dinno <tahadlrb7@gmail.com>
2025-09-05 19:16:59 +0330

    feat(nvim,plugin): Integrate lazygit with llm.nvim for commit messages

--- .config/nvim/lua/plugins/lazygit.lua
+++ .config/nvim/lua/plugins/lazygit.lua
@@ -16,4 +16,10 @@ return {
   keys = {
     { "<leader>lg", "<cmd>LazyGit<cr>", desc = "LazyGit" },
   },
+  config = function()
+    vim.keymap.set("t", "<C-c>", function()
+      vim.api.nvim_win_close(vim.api.nvim_get_current_win(), true)
+      vim.api.nvim_command "LLMAppHandler CommitMsg"
+    end, { desc = "AI Commit Msg" })
+  end,
 }
