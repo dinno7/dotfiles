c3857f99bfbb2f45bfadabacbecefc97c3cd61f7
dinno <tahadlrb7@gmail.com>
2025-09-05 19:13:00 +0330

    feat(nvim,plugin): Replace mini.notify with notify.nvim

--- .config/nvim/lua/plugins/mini/init.lua
+++ .config/nvim/lua/plugins/mini/init.lua
@@ -3,6 +3,5 @@ return {
   require "plugins.mini.mini-cursorword",
   require "plugins.mini.mini-jump2d",
   require "plugins.mini.mini-move",
-  require "plugins.mini.mini-notify",
   require "plugins.mini.mini-surround",
 }
---
+++ .config/nvim/lua/plugins/notify.lua
@@ -0,0 +1,26 @@
+return {
+  "rcarriga/nvim-notify",
+  lazy = false,
+  opts = {
+    -- default opts:
+    -- background_colour = "NotifyBackground",
+    -- fps = 30,
+    -- icons = {
+    --   DEBUG = "",
+    --   ERROR = "",
+    --   INFO = "",
+    --   TRACE = "✎",
+    --   WARN = "",
+    -- },
+    -- level = 2,
+    -- minimum_width = 50,
+    -- render = "default", -- default, minimal, simple, compact, wrapped-compact, wrapped-default
+    -- stages = "fade_in_slide_out",
+    -- time_formats = {
+    --   notification = "%T",
+    --   notification_history = "%FT%T",
+    -- },
+    -- timeout = 3000,
+    -- top_down = true,
+  },
+}
