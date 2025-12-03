cd4c17fae45b59e61fadb63b2d8246b4f887ff59
dinno <tahadlrb7@gmail.com>
2025-12-03 12:06:13 +0330

    feat(nvim): add Gitsigns mappings

--- nvim/.config/nvim/lua/mappings.lua
+++ nvim/.config/nvim/lua/mappings.lua
@@ -170,34 +170,6 @@ map("n", "[t", function()
 end, { desc = "Previous todo comment" })
 map("n", "<leader>td", "<cmd> TodoTelescope keywords=TODO,FIX,FIXME,BUG,FIXIT,ISSUE <cr>", { desc = "[L]ist of todos" })
 
--- NOTE: Gitsigns
-local gitsigns = require "gitsigns"
-
--- Actions
-map("n", "<leader>hS", gitsigns.stage_buffer)
-map("n", "<leader>hR", gitsigns.reset_buffer)
-map("n", "<leader>hp", gitsigns.preview_hunk)
-map("n", "<leader>hi", gitsigns.preview_hunk_inline)
-map("n", "<leader>hd", gitsigns.diffthis, {
-  desc = "[S]how diff",
-  silent = true,
-  noremap = true,
-})
-map("n", "<leader>hD", function()
-  gitsigns.diffthis "~"
-end)
-
-map("n", "<leader>lb", function()
-  gitsigns.blame_line { full = true }
-end, { desc = "[S]ee line blame", silent = true, noremap = true })
-map("n", "<leader>gb", gitsigns.blame, { desc = "[S]ee git commit complete blame", silent = true, noremap = true })
-map(
-  "n",
-  "<leader>tb",
-  gitsigns.toggle_current_line_blame,
-  { desc = "[T]oggle current line blame", silent = true, noremap = true }
-)
-
 -- NOTE: UFO(pkg) folding:
 map("n", "zR", require("ufo").openAllFolds, { desc = "UFO Open all folds" })
 map("n", "zM", require("ufo").closeAllFolds, { desc = "UFO Close all folds" })
@@ -241,3 +213,78 @@ map("n", "K", function()
 end, { desc = "Show errors if exist, else show fold preview, else Show documentation for what is under cursor" })
 
 map("v", "<Tab>", "%", { desc = "Jump to matching pair easily in visual mode" })
+
+-- NOTE: Gitsigns
+local gitsigns = require "gitsigns"
+
+-- Navigation
+map("n", "]c", function()
+  if vim.wo.diff then
+    vim.cmd.normal { "]c", bang = true }
+  else
+    gitsigns.nav_hunk "next"
+  end
+end)
+
+map("n", "[c", function()
+  if vim.wo.diff then
+    vim.cmd.normal { "[c", bang = true }
+  else
+    gitsigns.nav_hunk "prev"
+  end
+end)
+
+-- Actions
+map("n", "<leader>hs", gitsigns.stage_hunk, { desc = "[S]tage Hunk" })
+map("v", "<leader>hs", function()
+  gitsigns.stage_hunk { vim.fn.line ".", vim.fn.line "v" }
+end, { desc = "[S]tage Hunk" })
+
+map("n", "<leader>hr", gitsigns.reset_hunk, { desc = "[R]eset Hunk" })
+
+map("v", "<leader>hr", function()
+  gitsigns.reset_hunk { vim.fn.line ".", vim.fn.line "v" }
+end, { desc = "[R]eset Hunk" })
+
+map("n", "<leader>hS", gitsigns.stage_buffer, { desc = "[S]tage Buffer" })
+map("n", "<leader>hR", gitsigns.reset_buffer, { desc = "[R]eset Buffer" })
+map("n", "<leader>hp", gitsigns.preview_hunk, { desc = "[P]review Hunk" })
+map("n", "<leader>hi", gitsigns.preview_hunk_inline, { desc = "[P]review Hunk Inline" })
+
+map("n", "<leader>hb", function()
+  gitsigns.blame_line { full = true }
+end, {
+  desc = "[B]lame line",
+})
+
+map("n", "<leader>hd", gitsigns.diffthis, {
+  desc = "[D]iff this",
+})
+
+map("n", "<leader>hD", function()
+  gitsigns.diffthis "~"
+end, {
+  desc = "[D]iff this",
+})
+
+map("n", "<leader>hQ", function()
+  gitsigns.setqflist "all"
+end, {
+  desc = "[S]etqflist all",
+})
+map("n", "<leader>hq", gitsigns.setqflist, {
+  desc = "[S]etqflist",
+})
+
+-- Toggles
+map("n", "<leader>tb", gitsigns.toggle_current_line_blame, {
+  desc = "[T]oggle current line blame",
+})
+map("n", "<leader>tw", gitsigns.toggle_word_diff, {
+  desc = "[T]oggle word diff",
+})
+
+-- Text object
+map({ "o", "x" }, "ih", gitsigns.select_hunk, {
+  desc = "[S]elect Hunk",
+})
