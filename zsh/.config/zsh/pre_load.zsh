65edf1c3077f5f24cfd47240ca1318fc4de0271b
dinno <tahadlrb7@gmail.com>
2025-12-10 12:39:28 +0330

    feat(zsh): add pre_load.zsh

---
+++ zsh/.config/zsh/pre_load.zsh
@@ -0,0 +1,5 @@
+# NOTE: Load functions to has access to sourcefiles function
+[ -s "$DINNO_ZSH_DIR/functions.zsh" ] && source "$DINNO_ZSH_DIR/functions.zsh"
+
+# NOTE: load static envs
+sourcefiles "$DINNO_ZSH_DIR/envs.zsh" "$DINNO_ZSH_DIR/paths.zsh"
