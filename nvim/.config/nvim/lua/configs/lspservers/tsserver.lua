f2c2b27835ff99eaea74b17be18cca52cda5c350
dinno <tahadlrb7@gmail.com>
2026-01-03 22:08:00 +0330

    refactor(nvim): make tsserver configuration better

--- nvim/.config/nvim/lua/configs/lspservers/tsserver.lua
+++ nvim/.config/nvim/lua/configs/lspservers/tsserver.lua
@@ -1,19 +1,39 @@
-local function organize_imports()
-  local params = {
-    command = "_typescript.organizeImports",
-    arguments = { vim.api.nvim_buf_get_name(0) },
-    title = "",
-  }
-
-  local clients = vim.lsp.get_clients { name = "ts_ls" }
-  if #clients == 0 then
-    vim.notify("No ts_ls client found", vim.log.levels.ERROR)
-    return
-  end
-  local client = clients[1]
-  client:exec_cmd(params)
-  vim.notify("Imports sorted", vim.log.levels.INFO)
+local TSOrganizeImports = function()
+  vim.lsp.buf.code_action({
+    apply = true,
+    context = {
+      only = { "source.organizeImports.ts" },
+      diagnostics = {},
+    },
+  })
 end
+
+local TSRemoveUnused = function()
+  vim.lsp.buf.code_action({
+    apply = true,
+    context = {
+      only = { "source.removeUnused.ts" },
+      diagnostics = {},
+    },
+  })
+end
+
+-- local function organize_imports()
+--   local params = {
+--     command = "_typescript.organizeImports",
+--     arguments = { vim.api.nvim_buf_get_name(0) },
+--     title = "",
+--   }
+--
+--   local clients = vim.lsp.get_clients { name = "ts_ls" }
+--   if #clients == 0 then
+--     vim.notify("No ts_ls client found", vim.log.levels.ERROR)
+--     return
+--   end
+--   local client = clients[1]
+--   client:exec_cmd(params)
+--   vim.notify("Imports sorted", vim.log.levels.INFO)
+-- end
 
 -- vim.api.nvim_create_autocmd("BufWritePre", {
 --   pattern = { "*.ts", "*.tsx", "*.js", "*.jsx" },
@@ -21,15 +41,54 @@ end
 -- })
 
 local options = {
-  init_options = {
-    preferences = {
-      disableSuggestions = true,
+  single_file_support = true,
+  -- init_options = {
+  --   preferences = {
+  --     disableSuggestions = true,
+  --   },
+  -- },
+  commands = {
+    --   OrganizeImports = {
+    --     organize_imports,
+    --     description = "Organize Imports",
+    --   },
+    TSOrganizeImports = {
+      TSOrganizeImports,
+      description = "Organize Imports",
+    },
+    TSRemoveUnused = {
+      TSRemoveUnused,
+      description = "Remove unused",
     },
   },
-  commands = {
-    OrganizeImports = {
-      organize_imports,
-      description = "Organize Imports",
+
+  completions = {
+    completeFunctionCalls = true,
+  },
+
+  settings = {
+    javascript = {
+      inlayHints = {
+        includeInlayEnumMemberValueHints = true,
+        includeInlayFunctionLikeReturnTypeHints = true,
+        includeInlayFunctionParameterTypeHints = true,
+        includeInlayParameterNameHints = "all", -- 'none' | 'literals' | 'all';
+        includeInlayParameterNameHintsWhenArgumentMatchesName = true,
+        includeInlayPropertyDeclarationTypeHints = true,
+        includeInlayVariableTypeHints = true,
+      },
+    },
+
+    typescript = {
+      inlayHints = {
+        includeInlayEnumMemberValueHints = true,
+        includeInlayFunctionLikeReturnTypeHints = true,
+        includeInlayFunctionParameterTypeHints = true,
+        includeInlayParameterNameHints = "all", -- 'none' | 'literals' | 'all';
+        includeInlayParameterNameHintsWhenArgumentMatchesName = true,
+        includeInlayPropertyDeclarationTypeHints = true,
+        includeInlayVariableTypeHints = true,
+      },
     },
   },
 }
