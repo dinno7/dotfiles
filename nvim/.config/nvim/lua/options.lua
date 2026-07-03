94e20d9e3d7f2df216855d8e46b6af25f90928d2
dinno <tahadlrb7@gmail.com>
2026-07-03 16:27:37 +0330

    refactor(nvim): disable o.foldcolumn

--- nvim/.config/nvim/lua/options.lua
+++ nvim/.config/nvim/lua/options.lua
@@ -7,7 +7,7 @@ o.termbidi = true
 o.cursorlineopt = "both" -- to enable cursorline!
 
 -- INFO: Fold options
-o.foldcolumn = "auto:9"
+-- o.foldcolumn = "auto:1"
 o.foldlevel = 99
 o.foldlevelstart = 99
 o.foldenable = true
@@ -20,7 +20,6 @@ o.colorcolumn = "80"
 -- INFO: Enable spell checker
 o.spelllang = "en_us"
 o.spell = true
-
 
 -- INFO: Set custom snippets(vscode-like) path
 vim.g.vscode_snippets_path = vim.fn.stdpath "config" .. "/snippets"
