849e02e3d32e49d93fb2778ce4fd246dfeb51014
dinno <tahadlrb7@gmail.com>
2026-07-11 20:38:46 +0330

    refactor(nvim): active c-b in neoscroll

--- nvim/.config/nvim/lua/plugins/neoscroll.lua
+++ nvim/.config/nvim/lua/plugins/neoscroll.lua
@@ -5,7 +5,7 @@ return {
     mappings = { -- Keys to be mapped to their corresponding default scrolling animation
       "<C-u>",
       "<C-d>",
-      -- "<C-b>",
+      "<C-b>",
       "<C-f>",
       "<C-y>",
       "<C-e>",
@@ -15,7 +15,7 @@ return {
     },
     hide_cursor = true, -- Hide cursor while scrolling
     stop_eof = true, -- Stop at <EOF> when scrolling downwards
-    respect_scrolloff = false, -- Stop scrolling when the cursor reaches the scrolloff margin of the file
+    respect_scrolloff = true, -- Stop scrolling when the cursor reaches the scrolloff margin of the file
     cursor_scrolls_alone = true, -- The cursor will keep on scrolling even if the window cannot scroll further
     duration_multiplier = 0.5, -- Global duration multiplier
     easing = "linear", -- Default easing function -> linear, quadratic, cubic, quartic, quintic, circular, sine
