35c327965f3a419bb22446fc41a6d55c02cf9094
dinno <tahadlrb7@gmail.com>
2026-07-11 20:39:14 +0330

    refactor(nvim): remove [c keymap for treesitter context, & add his keymap in mappings.lua

--- nvim/.config/nvim/lua/plugins/nvim-treesitter-context.lua
+++ nvim/.config/nvim/lua/plugins/nvim-treesitter-context.lua
@@ -17,9 +17,5 @@ return {
       zindex = 20, -- The Z-index of the context window
       on_attach = nil, -- (fun(buf: integer): boolean) return false to disable attaching
     }
-
-    vim.keymap.set("n", "[c", function()
-      require("treesitter-context").go_to_context(vim.v.count1)
-    end, { silent = true, desc = "Go to prev context" })
   end,
 }
